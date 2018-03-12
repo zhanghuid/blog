b0abbf80c68aa6d12e744ac22ac3e21c8a14e4ca
huid <2921555618@qq.com>
2018-03-12 10:42:22 +0800

    Update and rename replace-aliyun-source.list.md to replace-aliyun-source-list.sh

---
+++ ubuntu/replace-aliyun-source-list.sh
@@ -0,0 +1,25 @@
+file="/etc/apt/sources.list.bak"
+if [ ! -f "$file" ]; then
+    echo '修改中。。。\n'
+    sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+    block="
+deb http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ xenial-backports main restricted universe multiverse
+    "
+    sudo echo "$block" > "/tmp/sources.list"
+    sudo mv /tmp/sources.list /etc/apt/sources.list ##修改成功了
+else
+    echo '已经是阿里的了\n' ##已经是阿里的了
+fi
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove
